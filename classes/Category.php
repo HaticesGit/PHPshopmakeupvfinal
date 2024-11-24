177999f4dee4f84a75612b8a14348fe10ce64383
Hatice Baldemir <hatice.baldemir@hotmail.com>
2024-11-24 16:30:37 +0100

    feat: add Categorie class

---
+++ classes/Category.php
@@ -0,0 +1,48 @@
+<?php
+namespace Hatice\makeupshop;
+
+class Category {
+    private $type;
+    private $product_id;
+
+
+    /**
+     * Get the value of type
+     */ 
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set the value of type
+     *
+     * @return  self
+     */ 
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of product_id
+     */ 
+    public function getProduct_id()
+    {
+        return $this->product_id;
+    }
+
+    /**
+     * Set the value of product_id
+     *
+     * @return  self
+     */ 
+    public function setProduct_id($product_id)
+    {
+        $this->product_id = $product_id;
+
+        return $this;
+    }
+}
