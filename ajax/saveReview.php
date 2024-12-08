15412fb3d1354e025a4254fa0e3c5d4ad4242354
Hatice Baldemir <hatice.baldemir@hotmail.com>
2024-12-08 17:42:07 +0100

    fix: xss protection in reviews (ajax)

--- ajax/saveReview.php
+++ ajax/saveReview.php
@@ -23,7 +23,7 @@
         // }
         $review = new Review();
         $review->setProduct_id($_POST['product_id']);
-        $review->setText($_POST['text']);
+        $review->setText(htmlspecialchars($_POST['text'], ENT_QUOTES, 'UTF-8'));
         $review->setUser_id($userId);
         //save
         $review->saveReview();
