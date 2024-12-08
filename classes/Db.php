87ac215caf29b7fe0a38022556d76b1652ed9860
Hatice Baldemir <hatice.baldemir@hotmail.com>
2024-12-08 19:37:04 +0100

    fix: cleaning up unnecessary comments

--- classes/Db.php
+++ classes/Db.php
@@ -1,26 +1,8 @@
 <?php 
     namespace Hatice\makeupshop;
-    // class Db{
-    //     private static $conn = null;  //statiek maken
-
-    //     public static function getConnection(){
-    //         //Db::getConnection();
-    //         if(self::$conn == null){//geen this want geen instantie
-    //             self::$conn =  new \PDO('mysql:dbname=makeupshop;host=localhost', "root", "root");
-    //             return self::$conn;
-    //     }
-    //     else{
-    //         return self::$conn;
-    //     } //als je connectie vraagt, staat er al 1 open? als er geen staat dan...
-    // }
-
     class Db {
         private static $conn = null;
         const SETTINGS = [
-            // "user" => "root", 
-            // "password" => "tOJmgRJtOljxohfnPqxNHWEjOUxvArYw", 
-            // "host" => "junction.proxy.rlwy.net:31590/railway", 
-            // "db" => "railway",
              "ssl_ca" => __DIR__ . "/CA.pem"
            
 
@@ -48,7 +30,6 @@
                     $options
                 );
             } catch (\PDOException $e) {
-                // Handle the exception (e.g., log the error, rethrow, etc.)
                 throw new \Exception("Database connection error: " . $e->getMessage());
             }
         }
